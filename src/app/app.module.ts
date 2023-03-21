84715e2231264dfd8711cee9cbbc23e37fd88800
MoON21 <voelknermoritz@outlook.de>
2023-03-21 20:44:20 +0100

    app component html + css +ts first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './components/test/test.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
+import { LottieAnimModule } from 'lottie-anim';
+import { FormsModule } from '@angular/forms';
 import { DestinationsComponent } from './components/destinations/destinations.component';
 import { DownloadComponent } from './components/download/download.component';
 import { HomeComponent } from './components/home/home.component';
@@ -25,8 +30,26 @@ import { PrivacyComponent } from './components/privacy/privacy.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    }),
+    LottieAnimModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+// AoT erfordert eine exportierte Funktion für Factories.  
+// LDie Lokalisierung wird aus dem Ordner assets/localization geladen.
+// Benennungsschema: en.json | de.json | fr.json
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/localization/', '.json');
+}
