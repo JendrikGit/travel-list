c59b5b7ad4da5e8d28936af0ce89f5fde2cd9d74
giterator21 <94552456+giterator21@users.noreply.github.com>
2023-03-22 17:01:42 +0100

    Configured Sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -9,34 +9,11 @@ import { LottieAnimComponent } from 'lottie-anim';
 })
 export class SidebarComponent {
 
-  @ViewChild('homeAnimation') homeAnimation: LottieAnimComponent | undefined;
-  @ViewChild('listAnimation') listAnimation: LottieAnimComponent | undefined;
-  @ViewChild('privacyAnimation') privacyAnimation: LottieAnimComponent | undefined;
-  @ViewChild('downloadAnimation') downloadAnimation: LottieAnimComponent | undefined;
 
   constructor() {
   }
 
   public ngAfterViewInit(): void {
-    this.attachHoverEvents();
-  }
-
-  private attachHoverEvents(): void {
-    document.getElementById('home-item')?.addEventListener('mouseover', () => {
-      this.homeAnimation?.goToAndPlay(0);
-    });
-
-    document.getElementById('list-item')?.addEventListener('mouseover', () => {
-      this.listAnimation?.goToAndPlay(0);
-    });
-
-    document.getElementById('privacy-item')?.addEventListener('mouseover', () => {
-      this.privacyAnimation?.goToAndPlay(0);
-    });
-
-    document.getElementById('download-item')?.addEventListener('mouseover', () => {
-      this.downloadAnimation?.goToAndPlay(0);
-    });
-  }
-
+   
+}
 }
