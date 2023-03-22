a83a4ed78dbffef8081955b86414623be50d9450
JendrikGit <jgweiss@gmx.de>
2023-03-22 16:50:06 +0100

    add home comp

---
+++ src/app/components/home/home.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/modules/shared.module';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    HomeRoutingModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class HomeModule { }
