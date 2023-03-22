99112f40e7c699ed82c2c6b63f32b5363240e586
giterator21 <94552456+giterator21@users.noreply.github.com>
2023-03-22 17:02:40 +0100

    Configured Shared Module

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,19 +1,31 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatInputModule} from '@angular/material/input'
+import {MatRippleModule} from '@angular/material/core';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatButtonModule} from '@angular/material/button';
+import {MatExpansionModule} from '@angular/material/expansion';
 
-
-
+/**
+ * Shared module that is imported by lazy loaded modules.
+ */
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ],
+  imports: [],
   exports: [
     CommonModule,
-    TranslateModule
+    TranslateModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatRippleModule,
+    MatChipsModule,
+    MatButtonModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: []
 })
-export class SharedModule { }
+export class SharedModule {
+}
