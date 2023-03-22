c9da5353f62da09c4ae4b34f9117d8fecf9748b2
Seb <sebastian.d.ludwig@icloud.com>
2023-03-22 12:23:10 +0100

    Changed App Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  
+  { path: 'home', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule) },
+  { path: 'destinations', loadChildren: () => import('./components/destinations/destination.module').then(m => m.DestinationModule) },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
