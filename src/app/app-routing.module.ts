78a53790855921e9e56ff432c686b4e5e037434b
giterator21 <94552456+giterator21@users.noreply.github.com>
2023-03-22 22:20:40 +0100

    Update home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'home', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule) },
   { path: 'destinations', loadChildren: () => import('./components/destinations/destinations.module').then(m => m.DestinationModule) },
+  { path: 'privacy', loadChildren: () => import('./components/privacy/privacy.module').then(m => m.PrivacyModule) },
+
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
