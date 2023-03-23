91fb905376dfee7c293ea1428955b9ed92e46f96
giterator21 <94552456+giterator21@users.noreply.github.com>
2023-03-23 22:12:22 +0100

    implemented theme toggler

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,105 +1,111 @@
 import { Component, ViewChild, NgZone } from '@angular/core';
-import { LottieAnimComponent } from 'lottie-anim';
 import { MatDialog } from '@angular/material/dialog';
+import { LottieAnimComponent } from 'lottie-anim';
+import { AppComponent } from 'src/app/app.component';
 import { IList } from 'src/app/models/list.interface';
 import { ITag } from 'src/app/models/tag.interface';
 import { DestinationService } from 'src/app/services/destination.service';
 import { ListsService } from 'src/app/services/lists.service';
 import { TagsService } from 'src/app/services/tags.service';
-import { ListDialogComponent } from '../list-dialog/list-dialog.component';
-import { TagsDialogComponent } from '../tags-dialog/tags-dialog.component';
-import { AppComponent } from 'src/app/app.component';
-
-
+import { AnimDialogComponent } from '../dialogs/anim-dialog/anim-dialog.component';
+import { ListDialogComponent } from '../dialogs/list-dialog/list-dialog.component';
+import { TagsDialogComponent } from '../dialogs/tags-dialog/tags-dialog.component';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-
 export class SidebarComponent {
 
   /**
-  * Gets or sets list of all available lists.
-  */
-  public availableLists: IList[] = [];
+   * Light theme by default.
+   */
+  public isLightTheme: boolean = true;
 
+  /**
+   * Gets or sets list of all available lists.
+   */
+  public availableLists: IList[] = [];
   /**
    * Gets or sets list if all available tags.
    */
   public availableTags: ITag[] = [];
-
   /**
-     * Gets or sets number of upcoming destinations.
-     */
+   * Gets or sets number of upcoming destinations.
+   */
   public destinationCount: number = 0;
-
   constructor(
     private listsService: ListsService,
     private tagsService: TagsService,
     private destinationsService: DestinationService,
     public dialog: MatDialog) {
+    // Get theme
+    let settings = localStorage.getItem('theme');
+    if (settings != undefined) {
+      this.isLightTheme = settings == 'light' ? true : false;
+      document.querySelector('html')?.setAttribute('data-theme', this.isLightTheme == true ? 'light' : 'dark');
+    }
+
     setTimeout(() => {
       this.initializeSidebar();
     }, 1000);
-
-
   }
-
   public ngAfterViewInit(): void {
-
-    private initializeSidebar(): void {
+  }
+  private initializeSidebar(): void {
     this.listsService.getLists().subscribe(result => {
       this.availableLists = result.map((item) => {
         return item._data;
       })
     });
-
     this.tagsService.getTags().subscribe(result => {
       this.availableTags = result.map((item) => {
         return item._data;
       })
     });
-
     this.destinationsService.getUpcomingDestinations().subscribe(result => {
       this.destinationCount = result.length;
     });
   }
-  
   /**
-* Opens dialog for adding new list.
-*/
-  public openListDialog(): void {
-    const dialogRef = this.dialog.open(ListDialogComponent);
-    dialogRef.afterClosed().subscribe(result => {
-    });
-  }
-
-  public dialog: MatDialog
-
-  /*Opens dialog for adding new list.
-  */
+   * Opens dialog for adding new list.
+   */
   public openListDialog(): void {
     const dialogRef = this.dialog.open(ListDialogComponent);
 
     dialogRef.afterClosed().subscribe(result => {
+  if (result != undefined) {
+        // Play animation
+        this.dialog.open(AnimDialogComponent);
+      }
     });
   }
-
-  /*
-  Opens dialog for adding new tag.
- */
+  /**
+   * Opens dialog for adding new tag.
+   */
   public openTagsDialog(): void {
     const dialogRef = this.dialog.open(TagsDialogComponent);
 
     dialogRef.afterClosed().subscribe(result => {
+if (result != undefined) {
+        // Play animation
+        this.dialog.open(AnimDialogComponent);
+      }
     });
   }
-  
   /**
    * Closes sidebar in narrow mode.
    */
   public closeSidebar(): void {
     AppComponent.isSidebarToggled = false;
+  }
+
+  /**
+   * Toggles application theme.
+   */
+  public toggleTheme(light: boolean): void {
+    this.isLightTheme = light;
+    localStorage.setItem('theme', this.isLightTheme == true ? 'light' : 'dark');
+    document.querySelector('html')?.setAttribute('data-theme', this.isLightTheme == true ? 'light' : 'dark');
   }
 }
