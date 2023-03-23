b38495035fb51fe99c680927fcc9d01a8b98b3b3
giterator21 <94552456+giterator21@users.noreply.github.com>
2023-03-23 20:45:41 +0100

    Resest Destinations + update dialog

--- src/app/components/destinations/destinations.component.ts
+++ src/app/components/destinations/destinations.component.ts
@@ -10,7 +10,6 @@ import { AnimDialogComponent } from '../dialogs/anim-dialog/anim-dialog.componen
 import { trigger, state, style, animate, transition } from '@angular/animations';
 import { ActivatedRoute } from '@angular/router';
 import { DetailsDialogComponent } from '../dialogs/details-dialog/details-dialog.component';
-
 @Component({
   selector: 'app-destinations',
   templateUrl: './destinations.component.html',
@@ -18,28 +17,24 @@ import { DetailsDialogComponent } from '../dialogs/details-dialog/details-dialog
   animations: [
     // the scale/fade-in/fade-out animation.
     trigger('scaleFadeAnimation', [
-
       // the "in" style determines the "resting" state of the element when it is visible.
- state('in', style({ opacity: 1 })),
-
+      state('in', style({ opacity: 1 })),
       // fade in when created. this could also be written as transition('void => *')
       transition(':enter', [
- style({ opacity: 0 }),
+        style({ opacity: 0 }),
         animate(200)
       ]),
-
       // fade out when destroyed. this could also be written as transition('void => *')
       transition(':leave',
-animate(300, style({ opacity: 0, transform: 'scale(0.7)' })))
+        animate(300, style({ opacity: 0, transform: 'scale(0.7)' })))
     ])
   ]
 })
 export class DestinationsComponent {
-
   /**
  * Gets or sets if data is being loaded.
  */
- public isLoading: boolean = false;
+  public isLoading: boolean = false;
   /**
    * Gets or sets list of all available lists.
    */
@@ -55,43 +50,36 @@ export class DestinationsComponent {
   /**
  * Gets or sets list of upcoming destinations.
  */
-public pastDestinations: IDestination[] = [];
+  public pastDestinations: IDestination[] = [];
   /**
    * Gets or sets selected destination.
    */
-public selectedDestination: IDestination | undefined;
-
+  public selectedDestination: IDestination | undefined;
   constructor(
     private route: ActivatedRoute,
     private listsService: ListsService,
     private tagsService: TagsService,
     private destinationsService: DestinationService,
     public dialog: MatDialog) {
-
     // Check passed query params to check if we 
     // need to load all destinations or from specified list / tag.
     this.route.queryParams.subscribe(params => {
       this.isLoading = true;
       let type = params['type'];
       let id = params['id'];
-
       this.selectedDestination = undefined;
-
+      
       setTimeout(() => {
         this.isLoading = false;
         this.initializeData(type, id);
       }, 1000);
-
     });
- }
-
+  }
   public ngOnInit(): void {
   }
- private initializeData(type: string | undefined = undefined, id: string | undefined = undefined): void {
+  private initializeData(type: string | undefined = undefined, id: string | undefined = undefined): void {
     this.getUpcomingDestinations(type, id);
     this.getPastDestinations(type, id);
-
-
     this.listsService.getLists().subscribe(result => {
       this.availableLists = result.map((item) => {
         return item._data;
@@ -111,36 +99,32 @@ public selectedDestination: IDestination | undefined;
       this.upcomingDestinations = result.map((val) => {
         return val._data;
       });
-
       if(type != undefined) {
         this.upcomingDestinations = this.upcomingDestinations.filter(x => type == 'list' ? (x.list.id == id) : (x.tag.id == id));
       }
-
-      this.selectedDestination = this.upcomingDestinations[0];
-    });
+  });
   }
 
   /**
    * Gets upcoming destinations.
    */
- private getPastDestinations(type: string | undefined = undefined, id: string | undefined = undefined): void {
+  private getPastDestinations(type: string | undefined = undefined, id: string | undefined = undefined): void {
     this.destinationsService.getPastDestinations(type, id).subscribe(result => {
       this.pastDestinations = result.map((val) => {
         return val._data;
       });
 
       if(type != undefined) {
-        this.upcomingDestinations = this.upcomingDestinations.filter(x => type == 'list' ? (x.list.id == id) : (x.tag.id == id));
+ this.pastDestinations = this.pastDestinations.filter(x => type == 'list' ? (x.list.id == id) : (x.tag.id == id));
       }
     });
   }
-
   /**
    * Deletes selected destination.
    */
   public deleteDestination(): void {
     this.destinationsService.deleteDestination(this.selectedDestination!);
-    // Play animation 
+    // Play animation
     this.dialog.open(AnimDialogComponent, {
       data: 'delete'
     });
@@ -150,7 +134,9 @@ public selectedDestination: IDestination | undefined;
    */
   public completeDestination(destination: IDestination, complete: boolean): void {
     this.destinationsService.completeDestination(destination, complete);
+    this.selectedDestination = undefined;
   }
+
   /**
    * Sets tag for the destination.
    * @param tag 
@@ -180,7 +166,6 @@ public selectedDestination: IDestination | undefined;
     let width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     if(width > 1200)
       return;
-
     this.dialog.open(DetailsDialogComponent, {
       data: { destination: destination}
     });
